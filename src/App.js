654da426f92d5bbf35de145a4b500e3aa2174e69
RamezCh <ramiz_chreide@outlook.com>
2024-08-29 06:18:55 +0300

    The React Quiz App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import DateCounter from './DateCounter';
+import Header from './Header';
+import Main from './Main';
 
 export default function App() {
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/5</p>
+        <p>Questions Count</p>
+      </Main>
     </div>
   );
 }
