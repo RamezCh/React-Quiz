10895a7d71a97effb9a9a0e9833abf06d6814f95
RamezCh <ramiz_chreide@outlook.com>
2024-09-01 23:45:53 +0300

    Starting a New Quiz

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from './Main';
 import Loader from './Loader';
 import Error from './Error';
 import StartScreen from './StartScreen';
+import Question from './Question';
 
 const initialState = {
   questions: [],
@@ -23,6 +24,11 @@ function reducer(state, action) {
       return {
         ...state,
         status: 'error',
+      };
+    case 'start':
+      return {
+        ...state,
+        status: 'active',
       };
     default:
       throw new Error('Action unknown');
@@ -47,7 +53,10 @@ export default function App() {
       <Main>
         {status === 'loading' && <Loader />}
         {status === 'error' && <Error />}
-        {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
+        {status === 'ready' && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === 'active' && <Question />}
       </Main>
     </div>
   );
