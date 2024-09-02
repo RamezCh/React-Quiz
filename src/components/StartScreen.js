3c89e498e4fa5a266e2858ed1b3928685b3ef5b9
RamezCh <ramiz_chreide@outlook.com>
2024-09-02 13:55:26 +0300

    Handling new Answers

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -2,7 +2,7 @@ function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
       <h2>Welcome to The React Quiz!</h2>
-      <h3>X questions to test your React mastery</h3>
+      <h3>{numQuestions} questions to test your React mastery</h3>
       <button
         onClick={() => dispatch({ type: 'start' })}
         className="btn btn-ui"
