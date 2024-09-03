1bd58c21e7069fd7908b755b040abdb9788255f4
RamezCh <ramiz_chreide@outlook.com>
2024-09-03 22:19:56 +0300

    useState vs useReducer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -24,3 +24,31 @@ function Timer({ dispatch, secondsRemaining }) {
 }
 
 export default Timer;
+
+/*
+useState:
+
+- single, independent pieces of state
+- logic didrectly in event handlers or effect spread all over one or multiple components
+- state update is done by calling setState
+- Imperative state updates
+- Easy to understand and use
+
+
+useReducer:
+
+- multiple related pieces of state and complex state
+- logic update lives in central place, decoupled from components
+- state updated by dispatching an action to a reducer
+- declarative state updates, complex state transitions are mapped to actions
+- More difficult to understand and implement
+
+
+When to use useReducer:
+
+Do we need one piece of state? Yes -> useState
+No -> do they need to be updated together?
+Yes -> are we willing to implement more slightly complex code? Yes -> useReducer
+
+Basically related states or too many states
+*/
